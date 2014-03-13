6eb23af3f5ad94b38322dba116f8b37a47250419
tsaikd <tsaikd@gmail.com>
2014-03-13 23:22:19 +0800

    update part kdintro with timeline style

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,25 +12,38 @@ module.exports = function(grunt) {
 				".tmp",
 				"<%= meta.dist %>/index/*.min.*",
 				"<%= meta.dist %>/index/*.tmp.*",
-				"<%= meta.dist %>/index.html"
+				"<%= meta.dist %>/index.html",
+				"<%= meta.dist %>/kdintro/*.min.*",
+				"<%= meta.dist %>/kdintro/*.tmp.*",
+				"<%= meta.dist %>/kdintro.html"
 			]
 		},
 		copy: {
 			index: {
 				src: "<%= meta.dist %>/index.src.html",
 				dest: "<%= meta.dist %>/index.html"
+			},
+			kdintro: {
+				src: "<%= meta.dist %>/kdintro.src.html",
+				dest: "<%= meta.dist %>/kdintro.html"
 			}
 		},
 		useminPrepare: {
 			index: {
 				src: "<%= meta.dist %>/index.html"
 			},
+			kdintro: {
+				src: "<%= meta.dist %>/kdintro.html"
+			},
 			options: {
 				dest: "<%= meta.dist %>"
 			}
 		},
 		usemin: {
-			html: ["<%= meta.dist %>/index.html"]
+			html: [
+				"<%= meta.dist %>/index.html",
+				"<%= meta.dist %>/kdintro.html"
+			]
 		},
 		version: {
 			dist: {
@@ -47,6 +60,38 @@ module.exports = function(grunt) {
 						return "app.run(['$templateCache', function($templateCache) {" + script + "}]);";
 					}
 				}
+			},
+			"kdintro": {
+				src: ["<%= meta.dist %>/kdintro/*.html"],
+				dest: "<%= meta.dist %>/kdintro/kdintro.ngtpl.tmp.js",
+				options: {
+					usemin: "<%= meta.dist %>/kdintro/kdintro.min.js",
+					bootstrap: function(module, script) {
+						return "app.run(['$templateCache', function($templateCache) {" + script + "}]);";
+					}
+				}
+			}
+		},
+		connect: {
+			server: {
+				options: {
+					port: 9000,
+					debug : true,
+					livereload : true
+				}
+			}
+		},
+		watch: {
+			livereload: {
+				options: {
+					livereload: true
+				},
+				files: [
+					"*.json",
+					"**/*.html",
+					"**/*.js",
+					"**/*.css"
+				]
 			}
 		}
 	});
@@ -54,5 +99,6 @@ module.exports = function(grunt) {
 	require("load-grunt-tasks")(grunt);
 	grunt.registerTask("build", ["copy", "ngtemplates", "useminPrepare", "concat", "cssmin", "uglify", "usemin", "version"]);
 	grunt.registerTask("default", ["clean", "sync", "build"]);
+	grunt.registerTask("dev", ["connect", "watch"]);
 
 };
