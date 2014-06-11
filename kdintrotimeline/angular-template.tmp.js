01f786e4cfd479e66b45bcd7e6c6a80eb45b12e1
tsaikd <tsaikd@gmail.com>
2014-06-11 15:13:38 +0800

    update intro timeline

--- kdintrotimeline/angular-template.tmp.js
+++ kdintrotimeline/angular-template.tmp.js
@@ -1,33 +1,19 @@
 app.run(['$templateCache', function($templateCache) {  'use strict';
 
   $templateCache.put('./kdintrotimeline/itemSpec.html',
-    "<item>\r" +
-    "\n" +
-    "\t<headline>html</headline>\r" +
-    "\n" +
-    "\t<startDate>yyyy-mm-dd</startDate>\r" +
-    "\n" +
-    "\t<endDate>yyyy-mm-dd</endDate>\r" +
-    "\n" +
-    "\t<repository>url</repository>\r" +
-    "\n" +
-    "\t<tags>\r" +
-    "\n" +
-    "\t\t<tag>string</tag>\r" +
-    "\n" +
-    "\t</tags>\r" +
-    "\n" +
-    "\t<text>html</text>\r" +
-    "\n" +
-    "\t<thumbnail>url</thumbnail>\r" +
-    "\n" +
-    "\t<assetimages>\r" +
-    "\n" +
-    "\t\t<assetimage>url</assetimage>\r" +
-    "\n" +
-    "\t</assetimages>\r" +
-    "\n" +
-    "</item>\r" +
-    "\n"
+    "<item>\n" +
+    "\t<headline>html</headline>\n" +
+    "\t<startDate>yyyy-mm-dd</startDate>\n" +
+    "\t<endDate>yyyy-mm-dd</endDate>\n" +
+    "\t<repository>url</repository>\n" +
+    "\t<tags>\n" +
+    "\t\t<tag>string</tag>\n" +
+    "\t</tags>\n" +
+    "\t<text>html</text>\n" +
+    "\t<thumbnail>url</thumbnail>\n" +
+    "\t<assetimages>\n" +
+    "\t\t<assetimage>url</assetimage>\n" +
+    "\t</assetimages>\n" +
+    "</item>\n"
   );
 }]);
